05f7f8cafa8aed92bfcd97c772cf84d8aabada28
swu664 <114163428+swu664@users.noreply.github.com>
2024-02-14 15:30:55 -0500

    added code for "get random" function

--- starter/script.js
+++ starter/script.js
@@ -110,7 +110,7 @@ function getPasswordOptions() {
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  return Math.floor(Math.random() * arr.length);
 }
 
 // Function to generate password with user input
