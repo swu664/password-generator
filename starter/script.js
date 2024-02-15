bb51e936171bffc31151a4a78ac84ae7d453e14d
swu664 <114163428+swu664@users.noreply.github.com>
2024-02-14 22:23:59 -0500

    added to the "generate password" function, calculated the number of characters assigned to each preference

--- starter/script.js
+++ starter/script.js
@@ -90,20 +90,20 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-  var pwdLengthElement = document.getElementById('password-length');
-  var pwdLength = pwdLengthElement.value;
+  let pwdLengthElement = document.getElementById('password-length');
+  let pwdLength = pwdLengthElement.value;
 
-  var preferenceElement1 = document.getElementById('preference1');
-  var uppercase = preferenceElement1.checked;
+  let preferenceElement1 = document.getElementById('preference1');
+  let uppercase = preferenceElement1.checked;
   
-  var preferenceElement2 = document.getElementById('preference2');
-  var lowercase = preferenceElement2.checked;
+  let preferenceElement2 = document.getElementById('preference2');
+  let lowercase = preferenceElement2.checked;
   
-  var preferenceElement3 = document.getElementById('preference3');
-  var numbers = preferenceElement3.checked;
+  let preferenceElement3 = document.getElementById('preference3');
+  let numbers = preferenceElement3.checked;
   
-  var preferenceElement4 = document.getElementById('preference4');
-  var punctuation = preferenceElement4.checked;
+  let preferenceElement4 = document.getElementById('preference4');
+  let punctuation = preferenceElement4.checked;
 
   console.log(pwdLength + uppercase + lowercase + numbers + punctuation)
 }
@@ -115,7 +115,74 @@ function getRandom(arr) {
 
 // Function to generate password with user input
 function generatePassword() {
+  getPasswordOptions();
 
+  let pwdPreferenceBoolean = [uppercase, lowercase, numbers, punctuation];
+  let pwdPreferenceArray = [];
+  let numPreferences = 0;
+  let draftPassword = new Array(pwdLength);
+  let actualPassword = new Array(pwdLength);
+
+  //calculate a balanced number of characters for each preference
+  //check the number of preferences
+  for (let i = 0; i < pwdPreferenceBoolean.length; i++) {
+    if (pwdPreferenceBoolean[i] != false) {
+      numPreferences+= 1;
+    }
+  }
+  let baseNumber = Math.floor(pwdLength / numPreferences); //minimum number of characters for the selected preferences
+  let numExtra = pwdLength % numPreferences; //the remainder
+  //declare variables
+  let numUppercase = 0;
+  let numLowercase = 0;
+  let numNumbers = 0;
+  let numPunctuation = 0;
+  //iterate through "pwd preference array" to assign the base number of characters
+  for (var i = 0; i < pwdPreferenceBoolean.length; i++) {
+    if (pwdPreferenceBoolean[i] = true) {
+      switch (i) {
+        case 0:
+          numUppercase = baseNumber; //update number of characters
+          pwdPreferenceArray.push(0); //note which type was selected following the cases
+          break;
+        case 1:
+          numLowercase = baseNumber;
+          pwdPreferenceArray.push(1);
+          break;
+        case 2:
+          numNumbers = baseNumber;
+          pwdPreferenceArray.push(2);
+          break;
+        case 3:
+          numPunctuation = baseNumber;
+          pwdPreferenceArray.push(3);
+          break;
+      }
+    }
+  }
+  //randomize the assignment of the additional characters
+  if (numExtra != 0) {
+    for (var i = 0; i < numExtra; i++) {
+      var randomIndex = Math.floor(Math.random() * numExtra);
+      switch (pwdPreferenceArray[randomIndex]) {
+        case 0:
+          numUppercase += 1;
+          break;
+        case 1:
+          numLowercase += 1;
+          break;
+        case 2:
+          numNumbers += 1;
+          break;
+        case 3:
+          numPunctuation += 1;
+          break;
+      }
+      pwdPreferenceArray.splice(randomIndex, 1); //remove the chosen character type
+      numExtra -= 1; //decrease the number of remaining extra characters
+    }
+  }
+  
 }
 
 // Get references to the #generate element
@@ -130,5 +197,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-getPasswordOptions();
 generateBtn.addEventListener('click', writePassword);
